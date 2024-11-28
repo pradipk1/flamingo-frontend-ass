eb346da1f5585bb176115b194e6ef2ce0e3fa6f3
Pradip Shukla <pradipshukla12699@gmail.com>
2024-11-28 17:45:51 +0530

    sober life and welcome description done!

---
+++ src/components/SoberLife/SoberLife.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SoberLife.css';
+
+function SoberLife() {
+  return (
+    <div className='SoberLifeMainDiv'>
+        <div className='SoberLifeImgDiv'>
+            <img src="https://s3-alpha-sig.figma.com/img/89e2/eaea/ed4dde922bd36ab3ff50b696e3dd193b?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=njPhpJdIs70nbce7UVrNuJitHmiJdrpYCmosBhGg6F06yKr19m5BM56XaLmfE~pw0zDuaBjcIwhQ23iBhl9qxU8jC2kWAYmoKaORJXDq5v-smSKD8SlSqvY0lJhc88Yf50-eP-HF3HHXR7tKzHQousgmqxLpi7LIxnrlBwCoxhVGOq5COCqeaDaNl1xcIsjGo6O9B2kvmAlZwXpPCT-lJX6mY-MD77wyhvGeompFy2HFq5WmXQs~QcMe8J8uF~URVVtM0xiKlD2Lv8FHskGNtawLjNoybeDCJuQJ6-EJ2Y393RIDiz9tZAP9C1Olxtyld7ElY2RFmcaS392G~xjxJQ__" alt="" />
+        </div>
+        
+        <div className='SoberLifeVedaImgDiv'>
+            <img src="https://s3-alpha-sig.figma.com/img/fa2c/9e71/a85421740de1030b15890acf557fd598?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NntOnBEW37fvmA7siHA1C7T9I0SWkJQl8TR6SyolvP7e97LLk2oUvOVC8g4V4aV1-TCWIja8aldzekucLTo8w9q6x3NwjAW8BaaSq-KY-WtiQg-ruc7dlGnIRcJKU8MGOXsfSlDx4jAN8SxXjNcXr04WC6KQICbBAdpydXa8EVnuQNDwbNvpF-jLECTlkobEarRZw5mHpV9mKsfJXoXTy5LXDIfW7SGMDOjNNwlRVO6azl3luvdzvEoH8bFU9toJEdXg26ptyR3MNzd69k8DD3ZFTgvQarW4-M0ROV5-QJsOLcYNgvu250rdaS1Y9fJOeNpdBOoX2mlrKJocAdH9eQ__" alt="" />
+        </div>
+
+        <div className='SoberLifeTextDiv'><h1>Sober Life</h1></div>
+
+        <div className='SoberLifePDiv'><p>The At-Home De-Addiction Program</p></div>
+        
+    </div>
+  )
+}
+
+export default SoberLife;
