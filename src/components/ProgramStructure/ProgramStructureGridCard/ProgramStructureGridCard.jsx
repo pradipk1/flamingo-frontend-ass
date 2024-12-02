8dc112b61fc85244a1a212a77bd2dd886b12cff1
Pradip Shukla <pradipshukla12699@gmail.com>
2024-12-02 17:41:09 +0530

    updated programStructureGridCard.jsx

--- src/components/ProgramStructure/ProgramStructureGridCard/ProgramStructureGridCard.jsx
+++ src/components/ProgramStructure/ProgramStructureGridCard/ProgramStructureGridCard.jsx
@@ -4,7 +4,7 @@ import './ProgramStructureGridCard.css';
 function ProgramStructureGridCard({data, ind}) {
   return (
     <div className='ProgramStructureGridCardMainDiv'>
-        <div className={ind%2 == 0 ? 'ProgramStructureGridCardImgDiv' : 'ProgramStructureGridCardImgDiv ProgramStructureGridCardImgDiv44'}>
+        <div className={ind%2 === 0 ? 'ProgramStructureGridCardImgDiv' : 'ProgramStructureGridCardImgDiv ProgramStructureGridCardImgDiv44'}>
             <img src={data.image} alt="program-structure-card-img" />
         </div>
         <div className='ProgramStructureGridCardDetailsDiv'>
