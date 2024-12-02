309e5d0127fc9bdf448ed1d7d5a1589f0bf9f7ef
Pradip Shukla <pradipshukla12699@gmail.com>
2024-12-02 17:00:13 +0530

    welcomeScript done!

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -8,16 +8,14 @@ import ProgramStructure from '../../components/ProgramStructure/ProgramStructure
 import WhatReceive from '../../components/WhatReceive/WhatReceive';
 import StartJourney from '../../components/StartJourney/StartJourney';
 import CostOfProgram from '../../components/CostOfProgram/CostOfProgram';
+import WelcomeScript from '../../components/WelcomeScript/WelcomeScript';
 
 function HomePage() {
   return (
     <div className='HomePageMainDiv'>
         <SoberLife />
 
-        <div className='WelcomeDescriptionDiv'>
-            <p>Welcome to the At-Home De-Addiction Program by Veda Rehabilitation & Wellness.<br />
-            This comprehensive 8-week therapeutic intervention is designed to help individuals overcome addiction<br />in the privacy and comfort of their own homes. By combining Veda’s renowned rehabilitation services<br />with LGH’s digital support, we offer a structured pathway to lasting recovery.</p>
-        </div>
+        <WelcomeScript />
 
         <TeamMembers />
 
---
+++ src/components/WelcomeScript/WelcomeScript.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WelcomeScript.css';
+
+function WelcomeScript() {
+  return (
+    <div className='WelcomeScriptMainDiv'>
+        <p>Welcome to the At-Home De-Addiction Program by Veda Rehabilitation & Wellness.<br />
+        This comprehensive 8-week therapeutic intervention is designed to help individuals overcome addiction<br />in the privacy and comfort of their own homes. By combining Veda’s renowned rehabilitation services<br />with LGH’s digital support, we offer a structured pathway to lasting recovery.</p>
+    </div>
+  )
+}
+
+export default WelcomeScript;
